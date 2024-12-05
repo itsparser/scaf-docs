f43c3efd25616edb38247b21962e68465a41e322
Vasanth kumar Kalaiselvan <itsparser@gmail.com>
2024-12-05 15:45:05 +0530

    Added the update componet from the application

---
+++ src/app/api/[username]/[templateId]/route.ts
@@ -0,0 +1,69 @@
+import { NextResponse } from "next/server";
+import { TemplateService } from "@/services/template.service";
+import { InfoService } from "@/services/info.service";
+
+export async function GET(
+  request: Request,
+  { params }: { params: { username: string; templateId: string } }
+) {
+  try {
+    const { username, templateId } = params;
+
+    // Get templates with pagination
+    const template = await TemplateService.getTemplateById(
+      `${username}/${templateId}`
+    );
+    const info = await InfoService.getInfoById(
+      `${username}/${templateId}:latest`
+    );
+
+    return NextResponse.json(
+      {
+        template,
+        info,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("Error fetching templates with info:", error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
+
+// Get info for a specific template
+export async function POST(
+  request: Request,
+  { params }: { params: { username: string; templateId: string } }
+) {
+  try {
+    const { username, templateId } = params;
+
+    // Get templates with pagination
+    const template = await TemplateService.getTemplateById(
+      `${username}/${templateId}`
+    );
+    if (!template) {
+      return NextResponse.json(
+        { error: "Template not found" },
+        { status: 404 }
+      );
+    }
+    const body = await request.json();
+
+    const infos = await InfoService.updateInfo(
+      `${username}/${templateId}:latest`,
+      body
+    );
+
+    return NextResponse.json(infos, { status: 200 });
+  } catch (error) {
+    console.error("Error fetching template info:", error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -1,105 +1,82 @@
 "use client";
 
 import Link from "next/link";
-import {useEffect, useState} from "react";
-import {ThemeToggle} from "@/components/theme-toggle";
-import {UserProfile} from "@/components/user-profile";
+import { useEffect, useState } from "react";
+import { ThemeToggle } from "@/components/theme-toggle";
+import { UserProfile } from "@/components/user-profile";
+import { useAuth } from "@/lib/auth";
+import { Button } from "@/components/ui/button";
+import { TemplateApi } from "@/lib/api/template";
 
 export function SiteHeader() {
-    const [hamburgerMenuIsOpen, setHamburgerMenuIsOpen] = useState(false);
+  const [hamburgerMenuIsOpen, setHamburgerMenuIsOpen] = useState(false);
+  const { user, getIdToken } = useAuth();
 
-    useEffect(() => {
-        const html = document.querySelector("html");
-        if (html) html.classList.toggle("overflow-hidden", hamburgerMenuIsOpen);
-    }, [hamburgerMenuIsOpen]);
+  const createTemplate = async () => {
+    try {
+      const token = await getIdToken();
+      const result = await TemplateApi.createTemplate(
+        {
+          _id: "itsparser/newv9",
+          name: "testing template",
+        },
+        token
+      );
 
-    useEffect(() => {
-        const closeHamburgerNavigation = () => setHamburgerMenuIsOpen(false);
-        window.addEventListener("orientationchange", closeHamburgerNavigation);
-        window.addEventListener("resize", closeHamburgerNavigation);
+      if (result.error) {
+        console.error("Failed to create template:", result.error);
+        return;
+      }
 
-        return () => {
-            window.removeEventListener("orientationchange", closeHamburgerNavigation);
-            window.removeEventListener("resize", closeHamburgerNavigation);
-        };
-    }, [setHamburgerMenuIsOpen]);
+      console.log("Template created:", result.data);
+    } catch (error) {
+      console.error("Error creating template:", error);
+    }
+  };
 
-    return (
-        <>
-            <header
-                className="fixed left-0 top-0 z-50 w-full translate-y-[-1rem] animate-fade-in border-b opacity-0 backdrop-blur-[12px] [--animation-delay:600ms]">
-                <div className="container flex h-[3.5rem] items-center justify-between">
-                    <Link className="text-md flex items-center" href="/">
-                        <b>Scaf</b>
-                    </Link>
+  useEffect(() => {
+    const html = document.querySelector("html");
+    if (html) html.classList.toggle("overflow-hidden", hamburgerMenuIsOpen);
+  }, [hamburgerMenuIsOpen]);
 
-                    <div className="ml-auto flex h-full items-center">
-                        <Link className="mr-6 text-sm" href="/templates">
-                            Browse Templates
-                        </Link>
-                        <ThemeToggle className="mr-6 text-sm"/>
-                        <UserProfile/>
-                    </div>
-                    {/*<button*/}
-                    {/*  className="ml-6 md:hidden"*/}
-                    {/*  onClick={() => setHamburgerMenuIsOpen((open) => !open)}*/}
-                    {/*>*/}
-                    {/*  <span className="sr-only">Toggle menu</span>*/}
-                    {/*  {hamburgerMenuIsOpen ? <XIcon /> : <AlignJustify />}*/}
-                    {/*</button>*/}
-                </div>
-            </header>
-            {/*<AnimatePresence>*/}
-            {/*  <motion.nav*/}
-            {/*    initial="initial"*/}
-            {/*    exit="exit"*/}
-            {/*    variants={mobilenavbarVariant}*/}
-            {/*    animate={hamburgerMenuIsOpen ? "animate" : "exit"}*/}
-            {/*    className={cn(*/}
-            {/*      `fixed left-0 top-0 z-50 h-screen w-full overflow-auto bg-background/70 backdrop-blur-[12px] `,*/}
-            {/*      {*/}
-            {/*        "pointer-events-none": !hamburgerMenuIsOpen,*/}
-            {/*      }*/}
-            {/*    )}*/}
-            {/*  >*/}
-            {/*    <div className="container flex h-[3.5rem] items-center justify-between">*/}
-            {/*      <Link className="text-md flex items-center" href="/">*/}
-            {/*        Scaf*/}
-            {/*      </Link>*/}
+  useEffect(() => {
+    const closeHamburgerNavigation = () => setHamburgerMenuIsOpen(false);
+    window.addEventListener("orientationchange", closeHamburgerNavigation);
+    window.addEventListener("resize", closeHamburgerNavigation);
 
-            {/*      <button*/}
-            {/*        className="ml-6 md:hidden"*/}
-            {/*        onClick={() => setHamburgerMenuIsOpen((open) => !open)}*/}
-            {/*      >*/}
-            {/*        <span className="sr-only">Toggle menu</span>*/}
-            {/*        {hamburgerMenuIsOpen ? <XIcon /> : <AlignJustify />}*/}
-            {/*      </button>*/}
-            {/*    </div>*/}
-            {/*    <motion.ul*/}
-            {/*      className={`flex flex-col md:flex-row md:items-center uppercase md:normal-case ease-in`}*/}
-            {/*      variants={containerVariants}*/}
-            {/*      initial="initial"*/}
-            {/*      animate={hamburgerMenuIsOpen ? "open" : "exit"}*/}
-            {/*    >*/}
-            {/*      {menuItem.map((item) => (*/}
-            {/*        <motion.li*/}
-            {/*          variants={mobileLinkVar}*/}
-            {/*          key={item.id}*/}
-            {/*          className="border-grey-dark pl-6 py-0.5 border-b md:border-none"*/}
-            {/*        >*/}
-            {/*          <Link*/}
-            {/*            className={`hover:text-grey flex h-[var(--navigation-height)] w-full items-center text-xl transition-[color,transform] duration-300 md:translate-y-0 md:text-sm md:transition-colors ${*/}
-            {/*              hamburgerMenuIsOpen ? "[&_a]:translate-y-0" : ""*/}
-            {/*            }`}*/}
-            {/*            href={item.href}*/}
-            {/*          >*/}
-            {/*            {item.label}*/}
-            {/*          </Link>*/}
-            {/*        </motion.li>*/}
-            {/*      ))}*/}
-            {/*    </motion.ul>*/}
-            {/*  </motion.nav>*/}
-            {/*</AnimatePresence>*/}
-        </>
-    );
+    return () => {
+      window.removeEventListener("orientationchange", closeHamburgerNavigation);
+      window.removeEventListener("resize", closeHamburgerNavigation);
+    };
+  }, [setHamburgerMenuIsOpen]);
+
+  return (
+    <>
+      <header className="fixed left-0 top-0 z-50 w-full translate-y-[-1rem] animate-fade-in border-b opacity-0 backdrop-blur-[12px] [--animation-delay:600ms]">
+        <div className="container flex h-[3.5rem] items-center justify-between">
+          <Link className="text-md flex items-center" href="/">
+            <b>Scaf</b>
+          </Link>
+
+          <div className="ml-auto flex h-full items-center">
+            <Link className="mr-6 text-sm" href="/templates">
+              Browse Templates
+            </Link>
+            {user && (
+              <Button
+                onClick={createTemplate}
+                variant="outline"
+                size="sm"
+                className="mr-6"
+              >
+                Create Template
+              </Button>
+            )}
+            <ThemeToggle className="mr-6 text-sm" />
+            <UserProfile />
+          </div>
+        </div>
+      </header>
+    </>
+  );
 }
